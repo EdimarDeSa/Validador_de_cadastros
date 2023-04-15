c91282c7f81b09f8b3d254a396af5480be013580
Edimar de Sá <edimarfreitas95@gmail.com>
2023-04-15 18:54:36 -0300

    MVP validador pandas
    Atualizei a estrutura que antes usava a biblioteca csv do python para melhorar a performance e redução de complexidade do código.

--- Modulos/log_panel.py
+++ Modulos/log_panel.py
@@ -1,32 +1,32 @@
-from Modulos import Toplevel, Tk, Text, Scrollbar
+from tkinter import Toplevel, Tk, Text, Scrollbar
+from tkinter.ttk import Progressbar
 
 
-class LogPanel:
+class LogPanel(Toplevel):
     def __init__(self, master: Tk, kw: dict):
-        kw.pop('master')
         kw['state'] = 'disabled'
         kw['wrap'] = 'word'
-        top_level = Toplevel(master)
-        top_level.geometry(f'800x500+{top_level.winfo_rootx()+600}+{top_level.winfo_rooty()}')
+        Toplevel.__init__(self, master)
+        self.geometry(f'800x500+{self.winfo_rootx()+600}+{self.winfo_rooty()}')
         
-        self._tela1 = Text(master=top_level, **kw)
-        scrollbar1 = Scrollbar(master=top_level, command=self._tela1.yview)
+        self._tela1 = Text(master=self, **kw)
+        scrollbar1 = Scrollbar(master=self, command=self._tela1.yview)
         self._tela1.place(relx=0, rely=0, relheight=1, relwidth=0.485)
         self._tela1.configure(yscrollcommand=scrollbar1.set)
         scrollbar1.place(relx=0.485, rely=0, relheight=1, relwidth=0.015)
 
-        self._tela2 = Text(master=top_level, **kw)
-        scrollbar2 = Scrollbar(master=top_level, command=self._tela2.yview)
+        self._tela2 = Text(master=self, **kw)
+        scrollbar2 = Scrollbar(master=self, command=self._tela2.yview)
         self._tela2.place(relx=0.5, rely=0, relheight=1, relwidth=0.485)
         self._tela2.configure(yscrollcommand=scrollbar2.set)
         scrollbar2.place(relx=0.985, rely=0, relheight=1, relwidth=0.015)
         
     def log_impressora(self, info, screen_number: int):
-        tela = None
-        if not screen_number:
-            tela = self._tela1
-        else:
-            tela = self._tela2
+        telas = {
+            0: self._tela1,
+            1: self._tela2
+        }
+        tela = telas[screen_number]
         tela.config(state='normal')
         tela.insert('end', f'{info}\n')
         tela.config(state='disabled')
